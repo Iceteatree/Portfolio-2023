004e4376dbec018bc03a26904da7595d6ccac43a
Alan K <alankow11@gmail.com>
2023-07-08 14:33:04 +0200

    [ADDED] Development: More animations, ground plane and chair

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,15 +1,52 @@
-import { OrbitControls } from '@react-three/drei';
+import {
+  ContactShadows, Environment, OrbitControls, Sky, Sparkles, Stars,
+} from '@react-three/drei';
 import { extend } from '@react-three/fiber';
+import { useControls } from 'leva';
 import AvatarModel from '../Avatar';
 
 extend({ OrbitControls });
 
 function Experience() {
+  const { animation }:any = useControls({
+    animation: {
+      value: 'Typing',
+      options: ['Typing', 'Falling', 'Standing'],
+    },
+  });
   return (
     <>
       <OrbitControls />
+      <Sky />
+      <Sparkles />
+      <Stars />
+
+      <Environment preset="sunset" />
       <group position-y={-1}>
-        <AvatarModel />
+        <ContactShadows opacity={0.38} scale={10} blur={1} far={10} resolution={256} color="#000000" />
+        <AvatarModel animation={animation} />
+        {
+          animation === 'Typing' && (
+            <>
+              <mesh
+                scale={[0.8, 0.5, 0.8]}
+                position-y={0.2}
+                position-z={-0.07}
+              >
+                <boxGeometry />
+                <meshStandardMaterial color="white" />
+              </mesh>
+              <mesh
+                scale={5}
+                rotation-x={-Math.PI * 0.5}
+                position-y={-0.02}
+              >
+                <planeGeometry />
+                <meshStandardMaterial color="white" />
+              </mesh>
+            </>
+          )
+        }
       </group>
     </>
   );
--- src/pages/avatar.tsx
+++ src/pages/avatar.tsx
@@ -1,10 +1,11 @@
+'useClient';
+
 import Experience from '@/components/Experience';
 import { Canvas } from '@react-three/fiber';
 
 function AvatarPage() {
   return (
     <Canvas style={{ width: '100vm', height: '100vh' }}>
-      <ambientLight intensity={1} />
       <pointLight position={[10, 10, 10]} />
       <camera position={[0, 2, 5]} />
       <Experience />
